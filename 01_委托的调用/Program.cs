096f92b24019e4bbdf5681f84f65c48200220168
俊华 蔡 <1032031912@qq.com>
2020-10-09 09:51:19 +0800

    委托节约代码

--- 01_委托的调用/Program.cs
+++ 01_委托的调用/Program.cs
@@ -6,10 +6,47 @@ using System.Threading.Tasks;
 
 namespace _01_委托的调用
 {
+    //声明委托类型
+    delegate void MyDel(int x);
+
+    //声明一个类
+    class MyInstObj
+    {
+        public void MyM1(int x)
+        {
+            Console.WriteLine("实例方法:x="+x);
+        }
+        public static void OtherM2(int x)
+        {
+            Console.WriteLine("静态方法:x=" + x);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            MyDel delVar, dVar;
+
+            //实例化对象
+            MyInstObj myInstObj = new MyInstObj();
+
+            //创建委托并保存引用 - 第一种方法
+            delVar = new MyDel(myInstObj.MyM1);//实例方法
+            dVar = new MyDel(MyInstObj.OtherM2);//静态方法
+
+            //创建委托引用包保存 - 第二种方法
+            delVar = myInstObj.MyM1; //实例方法
+            dVar = MyInstObj.OtherM2;//静态方法
+
+            //第二种方法是一种快捷方法，它仅仅由方法说明符组成。和方法一的代码是等价的。这种快捷语法能够工作是因为在方法名称和其相应的委托类型之间存在隐式转换
+
+            //调用
+            delVar(1);
+            dVar(2);
+
+            Console.ReadLine();
+
         }
     }
 }
---
+++ 02_委托实现节约代码/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_委托实现节约代码
+{
+
+    delegate int algorithm(int a, int b);
+
+    class Program
+    {
+        public static int TEST(int a, int b, algorithm function)
+        {
+            //调用委托
+            return function(a, b);
+        }
+        //加法
+        public static int addition(int a, int b)
+        {
+            return a + b;
+        }
+        //减法
+        public static int sub(int a, int b)
+        {
+            return a - b;
+        }
+        //乘法
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+        //除法
+        public static int division(int a, int b)
+        {
+            return a / b;
+        }
+        static void Main(string[] args)
+        {
+            int Number1 = 2;
+            int Number2 = 3;
+            //调用加法
+            Console.WriteLine(Number1 + "+" + Number2 + "=" + TEST(Number1, Number2, addition));
+            //调用减法
+            Console.WriteLine(Number1 + "-" + Number2 + "=" + TEST(Number1, Number2, sub));
+            //调用乘法
+            Console.WriteLine(Number1 + "*" + Number2 + "=" + TEST(Number1, Number2, Multiplication));
+            //调用除法
+            Console.WriteLine(Number1 + "/" + Number2 + "=" + TEST(Number1, Number2, division));
+
+            //
+            Console.ReadKey();
+
+        }
+    }
+}
