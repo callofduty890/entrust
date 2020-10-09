bdb371e933cd150b9679fd10a3473d210c20b818
俊华 蔡 <1032031912@qq.com>
2020-10-09 09:27:11 +0800

    添加委托

---
+++ 01_委托的调用/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_委托的调用
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
--- 10_9_代码/Program.cs
+++ 10_9_代码/Program.cs
@@ -1,15 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace _10_9_代码
 {
+    delegate void MyDel(int value);//声明委托
+
     class Program
     {
+
         static void Main(string[] args)
         {
+           void printLow(int value)
+        {
+            Console.WriteLine("{0}-Low Value", value);
+        }
+        void PrintHight(int value)
+        {
+            Console.WriteLine("{0}-Hight Value",value);
+        }         
+
+            void PrintNumber(double value)
+            {
+                Console.WriteLine("{0}-Value", value);
+            }
+            //创建一个随机数
+            Random rand = new Random();
+            int randValue = rand.Next(99);
+
+            //声明委托
+            MyDel del;
+            //使用委托
+
+            del = randValue < 50 ? new MyDel(printLow) : new MyDel(PrintHight);
+            
+            //执行委托
+            del(randValue);
+
+            Console.ReadKey();
         }
     }
 }
